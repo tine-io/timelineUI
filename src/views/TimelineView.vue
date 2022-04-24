3cff837fefd6263264bf6c099744109350d68e87
Peewee <peewee.manzer@outlook.de>
2022-04-24 17:47:15 +0200

    add timeline and map

--- src/views/TimelineView.vue
+++ src/views/TimelineView.vue
@@ -1,15 +1,54 @@
 <template>
-<v-sheet
-      id="scrolling-techniques-6"
-      class="overflow-y-auto"
-    >
-      <v-container style="height: 1000px;"></v-container>
-      <div>TimeLine</div>
-    </v-sheet>
+  <div id="scrolling-techniques-6">
+    <v-row id="row1" dense>
+      <v-col cols="3" id="rangeDatepicker">
+        <rangeDatepicker @dateChange="getDates"> </rangeDatepicker>
+        <v-timeline dense   :style="{ height: '90vh' }">
+          <v-timeline-item small>timeline item</v-timeline-item>
+          <v-timeline-item small> timeline item </v-timeline-item>
+          <v-timeline-item small>timeline item</v-timeline-item>
+        </v-timeline>
+      </v-col>
+      <v-col cols="9" :style="{ height: '100vh' }">
+        <l-map id="map" :zoom="10" :center="[52.54765, 13.446625]">
+          <l-tile-layer
+            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+          ></l-tile-layer>
+        </l-map>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
+import { LMap, LTileLayer } from "vue2-leaflet";
+import RangeDatepicker from "../components/RangeDatepicker";
+
 export default {
-  name: "Timeline"
+  name: "Timeline",
+  components: {
+    RangeDatepicker,
+    LMap,
+    LTileLayer,
+  },
+  data: () => ({}),
+  methods: {
+    async getDates(event) {
+      console.log(event);
+    },
+  },
 };
 </script>
+
+<style>
+#rangeDatepicker {
+  z-index: 2;
+}
+#row1 {
+  z-index: 0;
+}
+#map {
+  height: 100%;
+  z-index: 1;
+}
+</style>
